209d831f47673f61ed04d41fe4d839b4781a2241
RitaJ <ritaj777@gmail.com>
2021-03-27 21:46:57 +0200

    create individual custom object

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const forumApp = createApp(App)
+forumApp.mount('#app')
+
+//to create another Vue instance
+// const anotherVueApp = createApp(App)
+// anotherVueApp.component(name: 'NiceButton', component{})
+// anotherVueApp.use(AnotherPlugin)
+// anotherVueApp.mount('#another-app')
