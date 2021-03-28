78b217745fccc38023f93bf31809b9df0eae64c0
RitaJ <ritaj777@gmail.com>
2021-03-28 19:43:13 +0300

    Dynamic routes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@ import App from './App.vue'
 import PageHome from "./components/PageHome";
 // import * as VueRouter from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
+import PageThreadShow from "./components/PageThreadShow";
 
 const routes = [
-    { path: '/', name: 'Home', component: PageHome },
-    // { path: '/about', component: About },
+    {
+        path: '/',
+        name: 'Home',
+        component: PageHome
+    },
+    {
+        path: '/thread/:id',
+        name: 'ThreadShow',
+        component: PageThreadShow,
+        props: true
+    },
 ]
 
+// use Vue.Router when importing full plugin
 // const router = VueRouter.createRouter({
 //     // history: VueRouter.createWebHashHistory(),
 //     history: VueRouter.createWebHistory(),
