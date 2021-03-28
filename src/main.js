ae8a5252512459877b72e09a78787046c27cef39
RitaJ <ritaj777@gmail.com>
2021-03-28 18:25:05 +0300

    install vue-router and setup it

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from "./components/PageHome";
+// import * as VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+    { path: '/', name: 'Home', component: PageHome },
+    // { path: '/about', component: About },
+]
+
+// const router = VueRouter.createRouter({
+//     // history: VueRouter.createWebHashHistory(),
+//     history: VueRouter.createWebHistory(),
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
 const forumApp = createApp(App)
+forumApp.use(router)
 forumApp.mount('#app')
 
 //to create another Vue instance
